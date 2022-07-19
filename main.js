52a6fa70b8786ad5c4e6f03e08f2e5289c79b60a
Renzyyyy <renzcollincapena187@gmail.com>
2022-07-19 11:47:45 +0800

    Update responsive and update js

--- main.js
+++ main.js
@@ -8,6 +8,11 @@ let ShowBtn = document.querySelector('.ShowBtn');
 
 ShowBtn.addEventListener('click',()=>{
     HiddenNumber.classList.toggle('HiddenNumber_1')
+    alert(`You lose the Hidden number is ${HiddenNumber.innerText}`)
+
+    setTimeout(function clickShowHiddenNumber(){
+        location.reload();
+    },2000)
 })
 
 HiddenNumber.innerText = Math.floor(Math.random() * 50 + 1)
@@ -20,17 +25,41 @@ GoBtn.addEventListener('click',()=>{
     }else{
 
         NumberOfTry.innerText = NumberOfTry.innerText - 1;
-        if(NumberOfTry.innerText == 0){
-            alert("Talo ka Please Try again");
+        if(NumberOfTry.innerText == 0 && UserInput.value != HiddenNumber.innerText){
+            alert("You lose! Nice try Please try again 🤪");
             location.reload();
         }
 
         if(UserInput.value > HiddenNumber.innerText){
-            Hint.innerHTML = "<b>Your input is Greater than</b>"
+
+            setTimeout(function PopUp(){
+
+                Hint.style.backgroundColor = "rgba(0, 255, 157, 0.342)";
+                Hint.innerHTML = "<b>Your Guess number is too high</b>";
+
+                setTimeout(function closeHint(){
+                    Hint.style.background = 'none';
+                    Hint.innerHTML ='';
+                },3000)
+
+            },1000)
+
         }else if(UserInput.value < HiddenNumber.innerText){
-            Hint.innerHTML = "<b>Your input is Less than</b>"
+
+            setTimeout(function PopUp(){
+
+                Hint.style.backgroundColor = "rgba(255, 0, 34, 0.329)";
+                Hint.innerHTML = "<b>Your Guess number is too low</b>";
+
+                setTimeout(function closeHint(){
+                    Hint.style.background = 'none';
+                    Hint.innerHTML ='';
+                },3000)
+
+            },1000)
+            
         }else{
-            alert("Wow you guess the hidden number !!!");
+            alert("Congratulation you guess the hidden number !!!");
             location.reload();
         }
     }
